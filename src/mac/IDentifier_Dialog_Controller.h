d89d5a11a8dc1a9fde00e0726c5e29eefd5df250
Brendan Bolles <brendan@fnordware.com>
2015-02-25 10:10:57 -0800

    32-bit Cocoa fix

--- src/mac/IDentifier_Dialog_Controller.h
+++ src/mac/IDentifier_Dialog_Controller.h
@@ -1,5 +1,11 @@
 #import <Cocoa/Cocoa.h>
 
+#if !NSINTEGER_DEFINED
+typedef int NSInteger;
+#define NSINTEGER_DEFINED 1
+#endif
+
+
 @interface IDentifier_Dialog_Controller : NSObject {
     IBOutlet NSPopUpButton *idMenu;
     IBOutlet NSWindow *window;
